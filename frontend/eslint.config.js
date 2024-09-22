743eb4e2128b2c4543995c1478f812976245016c
Felix Schmidt <s224313@dtu.dk>
2024-09-22 16:54:04 +0200

    eslint.config.js sfnjke

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -8,7 +8,7 @@ export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx},{!node_modules/*}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
