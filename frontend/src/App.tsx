491c5c93044de7caf55bcb3eb79eb621199c705b
Felix Schmidt <s224313@dtu.dk>
2024-09-24 00:38:32 +0200

    UpperMenu UI overhaul

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,4 @@
 import './App.css'
-import {NavigationMenu} from "./Components/NavigationMenu.tsx";
-import {SearchField} from "./Components/SearchField.tsx";
-import DefaultButton from "./Components/Button.tsx";
-import InputField from "./Components/InputField.tsx";
 import {UpperMenu} from "./Components/UpperMenu.tsx";
 
 function App() {
@@ -13,10 +9,6 @@ function App() {
           </header>
           <main>
               <UpperMenu/>
-              <NavigationMenu/>
-              <DefaultButton text={"Hello World"} onClick={() => {}}/>
-
-              <InputField name="email" type="email" placeholder="Email"/>
           </main>
 
       </>
