732b51d97437ff700df080cfe9e74a91db90d889
Ali Masoud <alimohamadbro@gmail.com>
2024-09-25 23:03:30 +0200

    Search component done

---
+++ frontend/src/Pages/searchComponent.tsx
@@ -0,0 +1,14 @@
+import { SearchField } from "../Components/SearchField";
+
+export function SearchComponent() {
+    return (
+        <div className="search-container">
+            <div className="search-header">  
+                </div>
+            <div className="search-content">
+                <SearchField className="searchField"/>
+
+            </div>
+        </div>
+    );
+}
