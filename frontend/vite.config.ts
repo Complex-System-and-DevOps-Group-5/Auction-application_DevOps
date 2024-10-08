565f6354256173c0f7270dd62455d7b7888b8036
Zigalow <s224302@dtu.dk>
2024-10-08 23:06:31 +0200

    Added server port to vite.config.ts It's now possible to access website via docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: { // server is for development
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: { // preview is for to check out the production build
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
+
 
 
