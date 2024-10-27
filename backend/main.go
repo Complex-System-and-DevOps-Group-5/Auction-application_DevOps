9a70e8214950734bd843ddd909e7bf5204002f56
Zigalow <s224302@dtu.dk>
2024-10-27 20:29:12 +0100

    Added base setup for testing db connection

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
 	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5433
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "AuctionDb"
 )
 
 func main() {
@@ -11,5 +21,4 @@ func main() {
 	app := fiber.New()
 
 	log.Fatal(app.Listen(":4000"))
-
 }
