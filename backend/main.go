390d2f9fb87fa45dab0c7099981031b1eea1e13e
Zigalow <s224302@dtu.dk>
2024-10-27 20:31:21 +0100

    Added testcase to see if db is setup correctly. The testcase requires the db to be named AuctionDb and needs to have a table called list

--- backend/main.go
+++ backend/main.go
@@ -17,6 +17,38 @@ const (
 )
 
 func main() {
+	// Connect to the PostgreSQL database
+	connStr := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		dbname)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Perform a sample query
+	rows, err := db.Query("SELECT * FROM list")
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	// Iterate through the results
+	for rows.Next() {
+		var id int
+		var name string
+		if err := rows.Scan(&id, &name); err != nil {
+			panic(err)
+		}
+		fmt.Printf("ID: %d, Name: %s\n", id, name)
+
+	}
 	fmt.Println("Hello World")
 	app := fiber.New()
 
