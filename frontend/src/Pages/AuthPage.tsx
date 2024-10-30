85f593802dac5ee8d530274b9434da83e12772cd
Ali Masoud <alimohamadbro@gmail.com>
2024-10-30 20:59:35 +0100

    New inputField for token generation. Usestate for passwords and email. Using the new inputfield in login

--- frontend/src/Pages/AuthPage.tsx
+++ frontend/src/Pages/AuthPage.tsx
@@ -4,8 +4,11 @@ import DefaultButton from "../Components/DefaultButton.tsx";
 import {useLoginDispatch} from "../Context/LoginContext.tsx";
 import {useState} from "react";
 import {useNavigate} from "react-router-dom";
+import InputFieldToken from "../Components/InputField_Token.tsx";
 
 export function AuthPage() {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
     const dispatch = useLoginDispatch()
     const [isLoginPage, setIsLoginPage] = useState(true);
     const navigate = useNavigate();
@@ -20,8 +23,8 @@ export function AuthPage() {
                 // Login form
                 <>
                     <div className={"input-field-placement"}>
-                        <InputField name="email" type="email" placeholder="Email"/>
-                        <InputField name="password" type="password" placeholder="Password"/>
+                        <InputFieldToken name="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <InputFieldToken name="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     <div className="button-placement">
                         <DefaultButton
