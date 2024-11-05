f4332d5f9bb8940a3d7712be506d69f9600da5df
Felix Schmidt <s224313@dtu.dk>
2024-11-05 13:02:52 +0100

    demo submit bid  feature

--- frontend/src/Pages/ProductPage.tsx
+++ frontend/src/Pages/ProductPage.tsx
@@ -1,16 +1,46 @@
 import '../Styling/ProductPage.css'
 import watchList from "../assets/watchlist-icon.png"
-import {useState} from "react";
-import InputField from "../Components/InputField.tsx";
-import DefaultButton from "../Components/DefaultButton.tsx";
+import {useEffect, useState} from "react";
 
 export default function ProductPage () {
-    // most of these should be context
+    // from DOM:
     const [watchtListed, setWatchlisted] = useState(false)
+    const [bidAmount, setbidAmount] = useState(0)
+    // from backend:
+    const [amountOfBids, setamountOfBids] = useState(1)
+    const [winningBidAmount, setWinningBidAmount] = useState(999)
     const [sold, setSold] = useState(false)
-    const [bidPrice, setbidPrice] = useState(175)
-    const [amountOfBids, setamountOfBids] = useState(12)
+    const [minBidAmount, setMinBidAmount] = useState(1)
 
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+       setbidAmount(Number(event.target.value));  // Number() uhmm skriv bedre kode pls
+    }
+
+    async function handleBidSubmit(event: any){
+        event.preventDefault();
+        try {
+            await submitBid(bidAmount)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+    function submitBid(bid: number) {
+        //
+        return new Promise((resolve, reject): void => {
+            setTimeout(() => {
+                // get winningBidAmount from backend this will determine if promise will be resolved or rejected
+                if (bid >= winningBidAmount + minBidAmount){
+                    resolve(alert('A bit was sucessfully submitted: ' + bidAmount));
+                } else {
+                    reject(new Error('Backend did not approve your bid. Attempted to  submit: ' + bid));
+                }
+            }, 1500);
+        })
+    }
+    useEffect(() => {
+            setbidAmount(winningBidAmount+minBidAmount);
+    }, [winningBidAmount, minBidAmount])
     return(
         <div className="container">
             <div className="images">
@@ -31,13 +61,20 @@ export default function ProductPage () {
                 )
                 }
                 <p> {sold ? "SOLD" : "CURRENT BID"}</p>
-                <p>$ {bidPrice}&nbsp;<span style={{color: "gray"}}>{amountOfBids} bids</span></p>
+                <p>$ {winningBidAmount}&nbsp;<span style={{color: "gray"}}>{amountOfBids} bids</span></p>
                 {sold ? (
                     <span style={{color: "red", display: "flex", paddingTop: 10}}>Expired</span>
                 ) : (
                     <div className="bidSection">
-                        <InputField name="amount" type="amount" placeholder="Enter bid"></InputField>
-                        <DefaultButton text="Submit" color="#868686" onClick={() => {}}></DefaultButton>
+                        <form onSubmit={handleBidSubmit}>
+                            <input name="amount" type="number" value={bidAmount}
+                                   min={(winningBidAmount + minBidAmount).toString()}
+                                   pattern="[0-9]"
+                                   className="bidInput"
+                                   onChange={handleChange}
+                            />
+                            <button id="button">Submit</button>
+                        </form>
                     </div>
                 )}
             </div>
