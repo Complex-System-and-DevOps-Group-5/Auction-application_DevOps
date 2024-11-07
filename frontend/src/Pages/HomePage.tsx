cd43439aa394a3e76533deb180dc80dcc351773f
Zaid Sheikh <Zaidsh02@gmail.com>
2024-11-07 14:36:49 +0100

    Merge conflicts fixed

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,49 +1,11 @@
 import styles from "../Styling/ProductList.module.css";
-import DefaultButton from "../Components/Button.tsx";
-import { useEffect, useRef, useState } from "react";
+import DefaultButton from "../Components/DefaultButton.tsx";
 import { Product, ProductListProps } from "../Components/Product.ts";
 import { useFetch } from "../Components/Fetch.ts";
 
 export function HomePage() {
     const productsUrl: string = 'https://raw.githubusercontent.com/Complex-System-and-DevOps-Group-5/Auction-application_DevOps/refs/heads/improve/productList-component/frontend/src/Mockdata/ProductListItems.json';
-    //const [products, setProducts] = useState<Product[] | null> (null);
     const [products, isLoading, hasError] = useFetch(productsUrl);
-    //const [isLoading, setIsLoading] = useState(false);
-    //const [hasError, setHasError] = useState(false);
-
-    /*const hasFetchedProducts = useRef(false);
-
-    const fetchProducts = async (url: string) => {
-        setIsLoading(true);
-
-        try {
-            const response = await fetch(url);
-            const fetchedData = await response.json();
-            if(!response.ok) {
-                throw new Error('Network response was not ok, could not access: ' + url);
-            }
-            console.log('Data fetched correctly');
-            setProducts(fetchedData);
-            setIsLoading(false);
-        } catch (error) {
-            setHasError(true);
-            console.error(error);
-        }
-    }
-
-    useEffect(() => {
-        if(!hasFetchedProducts.current && products === null) {
-            fetchProducts(productsUrl)
-                .then(() => {
-                    console.log("Fetched products from API");
-                    hasFetchedProducts.current = true;
-                })
-                .catch((e) => {
-                        setHasError(true);
-                        console.error("could not fetch data: ", e)
-                });
-        }
-    },[products, productsUrl]);*/
 
     const productBoxItems = products && products.map((product: Product) => (
         !hasError && products !== null && (
@@ -53,7 +15,7 @@ export function HomePage() {
 
     return (
         <div className={styles.productList}>
-            <h1>TEST</h1>
+            {/*<HighlightedProduct product={products}/>*/}
             <div className={styles.productGrid}>
                 {hasError && <p>Error fetching products</p>}
                     {isLoading ? <div className="error">Loading...</div> : productBoxItems}
--- frontend/src/Pages/TrendingPage.tsx
+++ frontend/src/Pages/TrendingPage.tsx
@@ -1,12 +1,11 @@
-import ProductList from "../Components/ProductList.tsx";
-import productList from "../MockData/ProductListItems.json";
+
 
 export function TrendingPage() {
     return (
         <>
             <h1>Trending Page</h1>
 
-            <ProductList products={productList} />
+            {/*<ProductList products={productList} />*/}
         </>
     )
 }
