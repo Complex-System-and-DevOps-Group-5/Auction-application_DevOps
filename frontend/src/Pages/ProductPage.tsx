564bdbc5e048036f8d6f9878df00b36e73718f35
Felix Schmidt <s224313@dtu.dk>
2024-11-18 12:08:43 +0100

    feat(release2): update content

--- frontend/src/Pages/ProductPage.tsx
+++ frontend/src/Pages/ProductPage.tsx
@@ -39,7 +39,7 @@ export default function ProductPage () {
             console.log(err)
         }
     }
-
+// comment to trigger release
     function submitBid(bid: number) {
         //
         return new Promise((resolve, reject): void => {
