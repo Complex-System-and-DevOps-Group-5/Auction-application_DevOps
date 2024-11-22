645ceeb39f3d541d80adcaa763d0ddc5de0c85fb
Daniel Weper Jensen <s224291@student.dtu.dk>
2024-11-22 07:22:12 +0100

    Implemented most of the conditions, including the most useful and/or the most common

--- backend/conditions.go
+++ backend/conditions.go
@@ -14,22 +14,69 @@ type equalityCondition[T any] struct {
 	Value  T
 }
 
-func (c equalityCondition[T]) ToString() string {
-	return fmt.Sprintf("%s='%v'", c.Column, c.Value)
+type greaterCondition[T any] struct {
+	Column string
+	Value  T
 }
 
-type rangeCondition[T any] struct {
+type lessCondition[T any] struct {
+	Column string
+	Value  T
+}
+
+type nonEqualityCondition[T any] struct {
+	Column string
+	Value  T
+}
+
+type betweenCondition[T any] struct {
 	Column string
 	Start  T
 	End    T
 }
 
-func (c rangeCondition[T]) ToString() string {
-	return fmt.Sprintf("%s BETWEEN '%v' AND '%v'", c.Column, c.Start, c.End)
+type inCondition struct {
+	Column        string
+	Possibilities []string
 }
 
 type multiCondition struct {
 	Conditions []Condition
+}
+
+func (c equalityCondition[T]) ToString() string {
+	return fmt.Sprintf("%s='%v'", c.Column, c.Value)
+}
+
+func (c greaterCondition[T]) ToString() string {
+	return fmt.Sprintf("%s>'%v'", c.Column, c.Value)
+}
+
+func (c lessCondition[T]) ToString() string {
+	return fmt.Sprintf("%s<'%v'", c.Column, c.Value)
+}
+
+func (c nonEqualityCondition[T]) ToString() string {
+	return fmt.Sprintf("%s<>'%v'", c.Column, c.Value)
+}
+
+func (c betweenCondition[T]) ToString() string {
+	return fmt.Sprintf("%s BETWEEN '%v' AND '%v'", c.Column, c.Start, c.End)
+}
+
+func (c inCondition) ToString() string {
+	var sb strings.Builder
+
+	sb.WriteString("'")
+	sb.WriteString(c.Possibilities[0])
+	sb.WriteString("'")
+
+	for _, possibility := range c.Possibilities[1:] {
+		sb.WriteString(", '")
+		sb.WriteString(possibility)
+		sb.WriteString("'")
+	}
+	return fmt.Sprintf("%s IN (%s)", c.Column, sb.String())
 }
 
 func (c multiCondition) ToString() string {
@@ -48,8 +95,24 @@ func EqualityCondition[T any](column string, value T) Condition {
 	return equalityCondition[T]{Column: column, Value: value}
 }
 
-func RangeCondition[T any](column string, start T, end T) Condition {
-	return rangeCondition[T]{Column: column, Start: start, End: end}
+func GreaterCondition[T any](column string, value T) Condition {
+	return greaterCondition[T]{Column: column, Value: value}
+}
+
+func LessCondition[T any](column string, value T) Condition {
+	return lessCondition[T]{Column: column, Value: value}
+}
+
+func NonEqualityCondition[T any](column string, value T) Condition {
+	return nonEqualityCondition[T]{Column: column, Value: value}
+}
+
+func BetweenCondition[T any](column string, start T, end T) Condition {
+	return betweenCondition[T]{Column: column, Start: start, End: end}
+}
+
+func InCondition[T any](column string, possibilities ...string) Condition {
+	return inCondition{Column: column, Possibilities: possibilities}
 }
 
 func MultiCondition(conditions ...Condition) Condition {
