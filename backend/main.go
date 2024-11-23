0e0362dbe6b4e16d3fb85404276e4ab51ab8805b
Daniel Weper Jensen <s224291@student.dtu.dk>
2024-11-23 17:03:44 +0100

    Removed the unnecessary stuff from main.go

--- backend/main.go
+++ backend/main.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"log"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
 )
-
-type Bid struct {
-	Id           int       `json:"id"` // is this necessary?
-	Amount       int       `json:"amount"`
-	Status       int       `json:"status"` // i guess it's an int?
-	CreationTime time.Time `json:"creationTime" db:"creation_time"`
-	AuctionId    int       `json:"auctionId" db:"auction_id"`
-	BuyerId      int       `json:"buyerId" db:"buyer_id"`
-}
-
-type Image struct {
-	ImageUrl string `db:"string_url"`
-}
-
-type Test struct {
-	Id    sql.NullInt32 `db:"id" no-db:"insert"`
-	Name  string        `db:"name"`
-	Time  sql.NullTime  `db:"time" no-db:"insert"`
-	Extra uint
-}
 
 func main() {
 	ConnectToDatabase()
