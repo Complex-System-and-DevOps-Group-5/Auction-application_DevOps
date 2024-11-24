faa13ebb2a9d6835c4c0829e203d35c7b2b942bc
Daniel Weper Jensen <s224291@student.dtu.dk>
2024-11-24 13:34:14 +0100

    Added an easter egg

--- backend/main.go
+++ backend/main.go
@@ -16,6 +16,10 @@ func main() {
 
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("/home/user/Auction-application_DevOps/frontend/cursed.png")
+	})
+
 	app.Get("/product/:id", func(c *fiber.Ctx) error {
 		id, err := c.ParamsInt("id", -1)
 		if err != nil {
